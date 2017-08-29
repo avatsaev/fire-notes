23af9024ac6ae98119480e66652043bd91cd9d46
Aslan Vatsaev <aslan.vatsaev@ihu-strasbourg.eu>
2017-08-29 14:56:10 +0200

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyA9KjnUMg5hK3mg9Za9rAhYLH6KN8oJumY',
+    authDomain: 'intro-angular-demo.firebaseapp.com',
+    databaseURL: 'https://intro-angular-demo.firebaseio.com',
+    projectId: 'intro-angular-demo',
+    storageBucket: 'intro-angular-demo.appspot.com',
+    messagingSenderId: '524282417700'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA9KjnUMg5hK3mg9Za9rAhYLH6KN8oJumY',
+    authDomain: 'intro-angular-demo.firebaseapp.com',
+    databaseURL: 'https://intro-angular-demo.firebaseio.com',
+    projectId: 'intro-angular-demo',
+    storageBucket: 'intro-angular-demo.appspot.com',
+    messagingSenderId: '524282417700'
+  }
 };
